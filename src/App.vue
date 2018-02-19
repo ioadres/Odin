cfb6dadae59af99cdf69efca15a0bbe5343ff720
ioadres <andres.garcia.dev@ourlook.com>
2018-02-20 00:50:46 +0100

    Vue Routs Iniy

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1 v-show="showValue" >{{ msg }}</h1>
+    
+    <odin-footer></odin-footer>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import OdinFooter from '@/components/layout/Footer/Footer.vue'
+
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Hello World Vue.js App',
-      showValue: false
-    }
-  }
+  comments: {OdinFooter}
 }
 </script>
 
---
+++ src/components/layout/Footer/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  
+<h1>gollllla</h1>
+
+</template>
