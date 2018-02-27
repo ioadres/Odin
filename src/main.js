f4b5f5d8a79cc7307478ac0121651e3b29d79db0
ioadres <andres.garcia.dev@outlook.com>
2018-02-27 01:21:15 +0100

    custom bootstrap 4

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import BootstrapVue from '@/../node_modules/bootstrap-vue/dist/bootstrap-vue.esm'
 
 import App from './App.vue'
 import routes from '@/route'
 import getStore from '@/store'
 
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
 
 const router = new VueRouter({routes: routes})
 
--- src/page/LoginPage/LoginPage.vue
+++ src/page/LoginPage/LoginPage.vue
@@ -1,42 +1,21 @@
-<template lang="html">
-    <section class="hero is-fullheight">
-        <div class="container is-fluid">
-          <div class="column is-12">
-             <figure class="avatar">
-                <img src="https://placehold.it/128x128">
-              </figure>
-              <p class="subtitle has-text-grey">Please login to proceed.</p>
-          </div>
-          <div class="column is-3">       
-              <form>
-                <div class="field">
-                  <div class="control">
-                    <input class="input is-large" type="email" placeholder="Your Email" autofocus="">
-                  </div>
-                </div>
-
-                <div class="field">
-                  <div class="control">
-                    <input class="input is-large" type="password" placeholder="Your Password">
-                  </div>
-                </div>
-                <div class="field">
-                  <label class="checkbox">
-                    <input type="checkbox">
-                    Remember me
-                  </label>
-                </div>
-                <button class="button is-block is-info is-large is-fullwidth">Login</button>
-              </form>
-            </div>
+<template>
+  <div class="full-width-image">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <h1>Odin</h1>
+          <button class="btn btn-default btn-ingo">
+          vt
+          </button>
+        </div>
       </div>
-    </section>
+    </div>
+  </div>
 </template>
 
 <script src="./LoginPage.js">
 </script>
 
-<style src="./LoginPage.scss" module lang="scss">
-
+<style src="./LoginPage.scss" lang="scss">
 </style>
 
